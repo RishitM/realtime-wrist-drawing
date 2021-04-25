f7df2037c529a60ba40a69e969a46dc447fdd35d
RishitM <72683716+RishitM@users.noreply.github.com>
2021-04-25 10:49:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-function preload(){
+
+var leftWrist
+var rightWrist
+var nosex
+var nosey 
+ function preload(){
 
 };
 function setup(){
 canvas=createCanvas(500,600);
 canvas.center()
 video=createCapture(VIDEO);
-
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses)
 };
 function draw(){
-    let c = color(0, 0, 245);
-fill(c);
-noStroke();
-    square(100,100,100);
+ background("yellow")
+fill(0, 0, 245);
+    square(nosex,nosey,100);
 }
+function modelLoaded(){
+    console.log(ml5.version+' is ml5 version '+ ' Posenet Model is also loaded');
+}
+function gotPoses(results)
+{
+    if (results.length>0){
+console.log(results);
+leftWrist=results[0].pose.leftWrist.x;
+rightWrist=results[0].pose.rightWrist.x;
+nosex=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+console.log(leftWrist + "is x of left wrist");
+console.log(rightWrist + "is x of right wrist");
+console.log(nosex + "is x of nose" );
+console.log(nosey + "is y of nose");
+}}
